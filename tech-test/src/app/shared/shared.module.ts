b338cc91cee914d9860345ddbf8799973ab0d54d
Serhii Yermakov <sergey@2muchcoffee.com>
2022-12-14 17:00:56 +0200

    feat (tech-test:shared): update shared module exports

--- tech-test/src/app/shared/shared.module.ts
+++ tech-test/src/app/shared/shared.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   exports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
   ],
 })
 export class SharedModule { }
