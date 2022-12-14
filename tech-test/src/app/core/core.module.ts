d2522a33d527169d23725fc5718ec513222578c0
Serhii Yermakov <sergey@2muchcoffee.com>
2022-12-14 14:36:15 +0200

    feat (tech-test): add Angular Material UI library

--- tech-test/src/app/core/core.module.ts
+++ tech-test/src/app/core/core.module.ts
@@ -1,12 +1,14 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { NgxsStoreModule } from './ngxs/ngxs-store.module';
 
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     NgxsStoreModule,
   ],
