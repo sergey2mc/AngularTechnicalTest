94122e66838d01989244a20a02659b067b14a80b
Serhii Yermakov <sergey@2muchcoffee.com>
2022-12-14 14:28:12 +0200

    feat (tech-test:tasks:tasks-list): apply get-tasks resolver to tasks-list route

--- tech-test/src/app/tasks/tasks-list/tasks-list-routing.module.ts
+++ tech-test/src/app/tasks/tasks-list/tasks-list-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { GetTasksResolver } from '../../core/resolvers/get-tasks.resolver';
+
 import { TasksListComponent } from './tasks-list.component';
 
 const routes: Routes = [
   {
     path: '',
     component: TasksListComponent,
+    resolve: [GetTasksResolver],
   }
 ];
 
