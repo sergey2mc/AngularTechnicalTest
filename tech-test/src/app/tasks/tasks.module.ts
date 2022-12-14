942165e8a163f293b7723a8fb3d4ac2a13c22ac0
Serhii Yermakov <sergey@2muchcoffee.com>
2022-12-14 14:47:12 +0200

    feat (tech-test): add initial styles

--- tech-test/src/app/tasks/tasks.module.ts
+++ tech-test/src/app/tasks/tasks.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 
-import { SharedModule } from '../shared/shared.module';
-
 import { TasksComponent } from './tasks.component';
 import { TasksRoutingModule } from './tasks-routing.module';
 
 @NgModule({
-  imports: [
-    SharedModule,
-    TasksRoutingModule,
-  ],
+  imports: [TasksRoutingModule],
   declarations: [TasksComponent]
 })
 export class TasksModule { }
