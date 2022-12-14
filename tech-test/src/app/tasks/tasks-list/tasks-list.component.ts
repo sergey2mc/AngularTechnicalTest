325d2cbb2b5088da3688c21c992f2df7fdef81e4
Serhii Yermakov <sergey@2muchcoffee.com>
2022-12-14 17:01:35 +0200

    feat (tech-test:tasks:tasks-list): add tasks CRUD logic

--- tech-test/src/app/tasks/tasks-list/tasks-list.component.ts
+++ tech-test/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,29 +1,152 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { isArray as _isArray, isNumber as _isNumber } from 'lodash';
+import { BehaviorSubject, combineLatest, merge, Observable, Subject, Subscription } from 'rxjs';
+import { filter, map, scan, switchMap } from 'rxjs/operators';
 
 import { Task } from '../../core/models/task.model';
 import { TaskService } from '../../core/services/task.service';
+
+interface TableRow extends Task {
+  isEditing?: boolean;
+}
 
 @Component({
   selector: 'app-tasks-list',
   templateUrl: './tasks-list.component.html',
   styleUrls: ['./tasks-list.component.scss']
 })
-export class TasksListComponent implements OnInit {
-  tasksTableDataSource$: Observable<MatTableDataSource<Task>>;
+export class TasksListComponent implements OnInit, OnDestroy {
+  tasksTableDataSource$: Observable<MatTableDataSource<TableRow>>;
 
-  displayedColumns: Array<keyof Task> = ['label', 'description', 'category', 'done'];
+  setTaskEditMode$: BehaviorSubject<TableRow> = new BehaviorSubject(null);
+  addTask$: Subject<TableRow> = new Subject();
+  saveTask$: Subject<void> = new Subject();
+  deleteTask$: Subject<number> = new Subject();
+
+  setFormValueSubscription: Subscription;
+  resetFormValueSubscription: Subscription;
+  saveTaskSubscription: Subscription;
+  deleteTaskSubscription: Subscription;
+
+  taskColumns: Array<keyof Task> = ['label', 'description', 'category', 'done'];
+  actionColumns: string[] = ['actions'];
+  displayedColumns = [...this.taskColumns, ...this.actionColumns];
+
+  taskForm: FormGroup = new FormGroup({
+    id: new FormControl(null),
+    label: new FormControl(null, Validators.required),
+    description: new FormControl(null, Validators.required),
+    category: new FormControl(null, Validators.required),
+    done: new FormControl(null),
+  });
+
+  get labelControl(): FormControl {
+    return this.taskForm.get('label') as FormControl;
+  }
+
+  get descriptionControl(): FormControl {
+    return this.taskForm.get('description') as FormControl;
+  }
+
+  get categoryControl(): FormControl {
+    return this.taskForm.get('category') as FormControl;
+  }
+
+  get doneControl(): FormControl {
+    return this.taskForm.get('done') as FormControl;
+  }
 
   constructor(
-    private taskService: TaskService
+    private taskService: TaskService,
   ) {}
 
   ngOnInit(): void {
-    this.tasksTableDataSource$ = this.taskService.allTasks$.pipe(
-      map(tasks => new MatTableDataSource(tasks))
-    )
+    const tasksSource$: Observable<TableRow[]> = merge(
+      this.taskService.allTasks$,
+      this.addTask$,
+    ).pipe(
+      scan((acc: Task[], input: Task[] | TableRow) => {
+        if (_isArray(input)) {
+          return input as Task[];
+        } else {
+          return [
+            ...acc,
+            input,
+          ] as Task[]
+        }
+      }, []),
+    );
+
+    this.tasksTableDataSource$ = combineLatest(
+      tasksSource$,
+      this.setTaskEditMode$,
+    ).pipe(
+      map(([tasks, editingTask]: [TableRow[], Task]) => {
+        let modifiedRows: TableRow[] = tasks;
+        if (editingTask) {
+          modifiedRows = modifiedRows.map(task => ({ ...task, isEditing: task.id === editingTask.id }));
+        }
+
+        return new MatTableDataSource(modifiedRows);
+      })
+    );
+
+    this.setFormValueSubscription = this.setTaskEditMode$.pipe(
+      filter(task => !!task)
+    ).subscribe(({ isEditing, ...task }) => {
+      this.taskForm.setValue(task);
+    });
+
+    this.resetFormValueSubscription = this.setTaskEditMode$.pipe(
+      filter(task => !task)
+    ).subscribe(() => {
+      this.taskForm.reset();
+    });
+
+    this.saveTaskSubscription = this.saveTask$.pipe(
+      switchMap(() => {
+        const task = this.taskForm.value;
+        if (_isNumber(task.id)) {
+          return this.taskService.updateTask(task);
+        } else {
+          return this.taskService.createTask(task);
+        }
+      })
+    ).subscribe(() => {
+      // disable editing mode
+      this.setTaskEditMode$.next(null);
+    });
+
+    this.deleteTaskSubscription = this.deleteTask$.pipe(
+      filter(taskId => _isNumber(taskId))
+    ).subscribe(taskId => {
+      this.taskService.deleteTask(taskId);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.setFormValueSubscription.unsubscribe();
+    this.resetFormValueSubscription.unsubscribe();
+    this.saveTaskSubscription.unsubscribe();
+    this.deleteTaskSubscription.unsubscribe();
+  }
+
+  addTask() {
+    this.addTask$.next({ isEditing: true } as TableRow)
+  }
+
+  editTask(task: TableRow) {
+    this.setTaskEditMode$.next(task);
+  }
+
+  saveTask() {
+    this.saveTask$.next();
+  }
+
+  deleteTask(task: TableRow) {
+    this.deleteTask$.next(task.id);
   }
 }
--- tech-test/src/app/tasks/tasks-list/tasks-list.module.ts
+++ tech-test/src/app/tasks/tasks-list/tasks-list.module.ts
@@ -1,4 +1,7 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
 
 import { SharedModule } from '../../shared/shared.module';
@@ -10,6 +13,9 @@ import { TasksListRoutingModule } from './tasks-list-routing.module';
   imports: [
     SharedModule,
     MatTableModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
     TasksListRoutingModule,
   ],
   declarations: [TasksListComponent]
